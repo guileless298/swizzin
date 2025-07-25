b184d91409952af808d57370a876ca0cf54295e2
guileless298 <guileless298@gmail.com>
2025-07-25 15:32:38 +0100

    WIP

--- scripts/nginx/subdomain.sh
+++ scripts/nginx/subdomain.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
 
-# shellcheck disable=SC2016
-sed -Ei '
-/listen 443/,/^}/ s|server_name .*;|server_name ~^(?:(?<matched_subdomain>[^.]*)\.)?(?<matched_domain>.+)$;|g;
-/map $matched_subdomain $matched_domain {/,/}/d
+hostname=$(grep -m1 "server_name" /etc/nginx/sites-enabled/default | awk '{print $2}' | sed 's/;//g' | sed 's/\./\\./g')
+
+sed -Ei "
+/map \$host \$matched_subdomain/,/}/d;
+/map \$host \$matched_domain/,/}/d;
+1i\
+map \$host \$matched_subdomain {\
+    ~^[0-9]\..+$ \"panel\";\
+    ~^(?<matched_subdomain>[^.]+)\..+$ \$matched_subdomain;\
+    default \"panel\";\
+}\
+\
+map \$host \$matched_domain {\
+    ~^[0-9]\..+$ \$host;\
+    ~^[^.]+\.(?<matched_domain>.+)$ \$matched_domain;\
+    default \$host;\
+}
+s|server_name .*;|$hostname *.$hostname;|g;
 /root \/srv\/;/,/include/{//!d;};
 /root \/srv\/;/a\
   \
   location ~ ^/(?<service>[a-z]+)$ {\
-    return 301 $scheme://$service.$matched_domain$request_uri;\
+    return 301 \$scheme://\$service.\$matched_domain\$request_uri;\
   }\
   \
-  rewrite ^ "/$subdomain$uri" break;
-' /etc/nginx/sites-enabled/default
-
-cat >> /etc/nginx/sites-enabled/default << 'EOF'
-
-map $matched_subdomain $subdomain {
-    default $matched_subdomain;
-    ""      panel;
-}
-EOF
+  rewrite ^ \"/\$matched_subdomain\$uri\" break;
+" /etc/nginx/sites-enabled/default
--- scripts/remove/subdomain.sh
+++ scripts/remove/subdomain.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-set -x
+hostname=$(grep -m1 "server_name" /etc/nginx/sites-enabled/default | awk '{print $2}' | sed 's/;//g')
 
-server_name=$(grep -m1 "server_name" /etc/nginx/sites-enabled/default)
 sed -Ei "
-/listen 443/,/^}/ s|server_name .*;|$server_name|g;
-/root \/srv\/;/,/include/{//!d;}
+/listen 443/,/^}/ s|server_name .*;|server_name $hostname;|g;
+/root \/srv\/;/,/include/{//!d;};
 " /etc/nginx/sites-enabled/default
 
 rm /install/.subdomain.lock
