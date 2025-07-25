3fa6d90026659ca50f6db45333e6e303071da98b
guileless298 <guileless298@gmail.com>
2025-07-25 15:37:44 +0100

    WIP

--- scripts/nginx/subdomain.sh
+++ scripts/nginx/subdomain.sh
@@ -5,25 +5,25 @@ hostname=$(grep -m1 "server_name" /etc/nginx/sites-enabled/default | awk '{print
 sed -Ei "
 /map \$host \$matched_subdomain/,/}/d;
 /map \$host \$matched_domain/,/}/d;
-1i\
-map \$host \$matched_subdomain {\
-    ~^[0-9]\..+$ \"panel\";\
-    ~^(?<matched_subdomain>[^.]+)\..+$ \$matched_subdomain;\
-    default \"panel\";\
-}\
-\
-map \$host \$matched_domain {\
-    ~^[0-9]\..+$ \$host;\
-    ~^[^.]+\.(?<matched_domain>.+)$ \$matched_domain;\
-    default \$host;\
+1i\\
+map \$host \$matched_subdomain {\\
+    ~^[0-9]\..+$ \"panel\";\\
+    ~^(?<matched_subdomain>[^.]+)\..+$ \$matched_subdomain;\\
+    default \"panel\";\\
+}\\
+\\
+map \$host \$matched_domain {\\
+    ~^[0-9]\..+$ \$host;\\
+    ~^[^.]+\.(?<matched_domain>.+)$ \$matched_domain;\\
+    default \$host;\\
 }
-s|server_name .*;|$hostname *.$hostname;|g;
+s|server_name .*;|server_name $hostname *.$hostname;|g;
 /root \/srv\/;/,/include/{//!d;};
-/root \/srv\/;/a\
-  \
-  location ~ ^/(?<service>[a-z]+)$ {\
-    return 301 \$scheme://\$service.\$matched_domain\$request_uri;\
-  }\
-  \
+/root \/srv\/;/a\\
+  \\
+  location ~ ^/(?<service>[a-z]+)$ {\\
+    return 301 \$scheme://\$service.\$matched_domain\$request_uri;\\
+  }\\
+  \\
   rewrite ^ \"/\$matched_subdomain\$uri\" break;
 " /etc/nginx/sites-enabled/default
