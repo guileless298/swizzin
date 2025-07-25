c4f30e48c3ef288fa37e7fdab5889eb1468d7daf
guileless298 <guileless298@gmail.com>
2025-07-25 15:12:56 +0100

    WIP

--- scripts/nginx/subdomain.sh
+++ scripts/nginx/subdomain.sh
@@ -3,13 +3,9 @@
 # shellcheck disable=SC2016
 sed -Ei '
 /listen 443/,/^}/ s|server_name .*;|server_name ~^(?:(?<matched_subdomain>[^.]*)\.)?(?<matched_domain>.+)$;|g;
+/map $matched_subdomain $matched_domain {/,/}/d
 /root \/srv\/;/,/include/{//!d;};
 /root \/srv\/;/a\
-  \
-  map $matched_subdomain $subdomain {\
-    default $matched_subdomain;\
-    ""      panel;\
-  }\
   \
   location ~ ^/(?<service>[a-z]+)$ {\
     return 301 $scheme://$service.$matched_domain$request_uri;\
@@ -17,3 +13,11 @@ sed -Ei '
   \
   rewrite ^ "/$subdomain$uri" break;
 ' /etc/nginx/sites-enabled/default
+
+cat >> /etc/nginx/sites-enabled/default << 'EOF'
+
+map $matched_subdomain $subdomain {
+    default $matched_subdomain;
+    ""      panel;
+}
+EOF
