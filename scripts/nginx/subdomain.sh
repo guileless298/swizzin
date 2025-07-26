7f3a3fad88645505f32b69292cd8cd566608f92f
guileless298 <guileless298@gmail.com>
2025-07-26 11:32:40 +0100

    WIP

--- scripts/nginx/subdomain.sh
+++ scripts/nginx/subdomain.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 hostname=$(grep -m1 "server_name" /etc/nginx/sites-enabled/default | awk '{print $2}' | sed 's/;//g')
-escaped_hostname=${hostname//./\\.};
+escaped_hostname=${hostname//./\\\\.};
 
 set -x
 
