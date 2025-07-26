c17b10c42a248ab2db1941510e579942dd1611ae
guileless298 <guileless298@gmail.com>
2025-07-26 12:19:25 +0100

    WIP

--- scripts/install/subdomain.sh
+++ scripts/install/subdomain.sh
@@ -47,8 +47,8 @@ _install() {
     echo_progress_done
 
     echo_progress_start "Installing python dependencies"
-    /opt/.venv/swizzin/bin/pip install --upgrade pip wheel >> ${log} 2>&1
-    /opt/.venv/swizzin/bin/pip install -r /opt/swizzin/requirements.txt >> ${log} 2>&1
+    /opt/.venv/subdomain-auth/bin/pip install --upgrade pip wheel >> ${log} 2>&1
+    /opt/.venv/subdomain-auth/bin/pip install -r /opt/subdomain-auth/requirements.txt >> ${log} 2>&1
     echo_progress_done
 
     touch /install/.subdomain.lock
