3d67c5f822150a4016d2572c196b6c5b97139eec
guileless298 <guileless298@gmail.com>
2025-07-30 18:32:18 +0100

    WIP

--- scripts/nginx/panel.sh
+++ scripts/nginx/panel.sh
@@ -36,11 +36,17 @@ if [[ -f /install/.subdomain.lock ]]; then
     /^[[:space:]]*proxy_pass/ s|:8333;|:8333$request_uri;|;
     ' /etc/nginx/apps/panel.conf
 
+    sed -E '/^include \/etc\/nginx\/snippets\/subauth\.conf;$/d' /etc/nginx/apps/panel.conf > /etc/nginx/apps/.panel.conf
+
     sed -E '
     s|^location /panel/ \{|location = /panel/login {|;
     1i\
 
-    ' /etc/nginx/apps/panel.conf > /etc/nginx/apps/.panel.conf
-    cat /etc/nginx/apps/.panel.conf >> /etc/nginx/apps/panel.conf
+    ' /etc/nginx/apps/.panel.conf > /etc/nginx/apps/panel.conf
+    sed -E '
+    s|^location /panel/ \{|location /panel/static/ {|;
+    1i\
+
+    ' /etc/nginx/apps/.panel.conf > /etc/nginx/apps/panel.conf
     rm /etc/nginx/apps/.panel.conf
 fi
